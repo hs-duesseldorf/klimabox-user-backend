6f1bb1c396cbbdfaf60ab8983cbfd092450d4e73
Philipp Doll <philipp.doll@9elments.com>
2021-09-29 10:01:23 +0200

    try debug

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -7,6 +7,7 @@ module Api
       def authenticate_cookie
         token = cookies.signed[:jwt]
         decoded_token = JsonWebToken.decode(token)
+        puts(token, decoded_token)
         if decoded_token
           user = User.find_by(id: decoded_token["user_id"])
         end
